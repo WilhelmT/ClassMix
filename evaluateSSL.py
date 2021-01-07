e9dc847e59f8134202afb620b5dcca3fe1f975c5
Nikolaas Steenbergen <nikolaas.steenbergen@gmail.com>
2021-01-07 15:58:34 +0100

    Update evaluateSSL.py
    fix miou computation

--- evaluateSSL.py
+++ evaluateSSL.py
@@ -199,7 +199,7 @@ def evaluate(model, dataset, ignore_label=250, save_output_images=False, save_di
 
             output = output.transpose(1,2,0)
             output = np.asarray(np.argmax(output, axis=2), dtype=np.int)
-
+            data_list.append([gt.reshape(-1), output.reshape(-1)])
             if save_output_images:
                 if dataset == 'pascal_voc':
                     filename = os.path.join(save_dir, '{}.png'.format(name[0]))
